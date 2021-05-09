198b40ae1375270004ee243db482f878fc1e895c
Leonel Garcia <l.garcia3@estudiantes.matcom.uh.cu>
2021-05-08 21:18:35 -0400

    fix controller imports and stuff

--- src/mri_controller.py
+++ src/mri_controller.py
@@ -1,39 +1,7 @@
-from ..mri.src.iomethods import load_documents, load_queries
-from ..mri.src.mri import IRM
-from ..mri.src.document import Document
+from mri.src.iomethods import load_documents, load_queries
+from mri.src.mri import IRM
+from mri.src.document import Document
 from typing import List
-
-# # train
-# t = load_documents(cisi=False)
-
-# print(len(t))
-
-# # examples
-# f = load_queries(cisi=False)
-
-# print(len(f))
-
-# model = IRM()
-# # model.add(t[0])
-
-# # load traing documents
-# for d in t:
-#     model.add(d)
-
-# # print(model.index.terms)
-# print('words:', len(model.index.terms))
-
-# # load 1st example
-# c = model.run_query(f[0].query)
-
-# print(c)
-
-# # search for accurated predictions
-# for j, (i, _) in enumerate(c):
-#     if i in f[0].relevant_documents:
-#         print("acierto en ", j)
-
-# print(f[0].relevant_documents)
 
 
 class MriController:
@@ -41,23 +9,24 @@ class MriController:
     CRAN = 'CRAN'
 
     def _init_db(self, dataset: str):
-        self._documents = load_documents(path='../mri/datasets/', cisi=self.CISI)
-        self._queries = load_queries(path='../mri/datasets/', cisi=self.CISI)
+        self._documents = load_documents(path='./mri/datasets/', dataset=self.CISI)
+        self._queries = load_queries(path='./mri/datasets/', dataset=self.CISI)
         self._model = IRM()
         self.current_dataset = dataset
 
-    def __init__(self, datasets: String = [MriController.CISI, MriController.CRAN]):
+    def __init__(self, datasets: List[str] = None):
+        self.datasets = datasets if datasets is None else [self.CISI, self.CRAN]
+        self._model = None
         self._init_db(self.CISI)
-        self.datasets = datasets
 
-    def change_dataset(self, name: str = MriController.CRAN):
+    def change_dataset(self, name: str = CRAN):
         if name in self._datasets:
             self._init_db(name)
             return self._documents
         return []
 
     def execute_query(self, query: str) -> List[Document]:
-        return model.run_query(query, top=5)
+        return self.model.run_query(query, top=5)
 
     def get_document_abstract(self, title):
         return [d for d in self._documents if title == d.title]
